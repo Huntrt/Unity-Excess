db8562c4f71f61ed0449c03c0b210603aa8cbff4
Huntrt <huntrt.business@outlook.com>
2021-12-14 00:19:58 +0700

    Remove the old Pooling system

--- Assets/Excess/Gameplay/Pool.cs
+++ Assets/Excess/Gameplay/Pool.cs
@@ -1,30 +1,28 @@
-using System.Collections.Generic; using UnityEngine;
+using System.Collections.Generic; 
+using UnityEngine;
 
-//? Just F2 the component name if wanted to rename to "Pool" or anything
-public class AdvancedPool : MonoBehaviour
+public class Pool : MonoBehaviour
 {
 	//The pool contain all the object has create
-	public List<GameObject> objectsPool;
-
+	public List<GameObject> objectsPool = new List<GameObject>();
 	//Turn this script into singleton
-    public static AdvancedPool get; void Awake() {get = this;}
+    public static Pool s; void Awake() {s = this;}
 
-	//Create an clean new list for object pool
-	void Start() {objectsPool = new List<GameObject>();}
-
-	//Create the wanted object of whatever call this function with position, rotation and active it
-    public GameObject Create(GameObject Need,Vector3 Position,Quaternion Rotation,bool Active = true)
+	//Create the object needed with wanted position, rotation and does it auto active upon create?
+    public GameObject Create(GameObject Need, Vector3 Position, Quaternion Rotation, bool Active = true)
     {
-		/// If there is object in pool
+		///If there is unactive object in pool
         if(objectsPool.Count > 0)
 		{
 			//Go through all the object in pool in reverse order
 			for (int i = objectsPool.Count-1; i >= 0; i--)
 			{
-				//Remove an null object from bool
-				if(objectsPool[i] == null) {objectsPool.RemoveAt(i);}
+				//Remove any null object left from pool then skip it from being check
+				if(objectsPool[i] == null) {objectsPool.RemoveAt(i); continue;}
+				//Remove the "(Clone)" out of gameobject from pool name
+				string objectName = objectsPool[i].name.Replace("(Clone)","");
 				//If there is an unactive object in pool with the same name of object need to get
-				else if(!objectsPool[i].activeInHierarchy && Need.name+"(Clone)" == objectsPool[i].name)
+				if(!objectsPool[i].activeInHierarchy && string.Equals(Need.name, objectName))
 				{
 					//Set it position
 					objectsPool[i].transform.position = Position;
@@ -37,12 +35,12 @@ public class AdvancedPool : MonoBehaviour
 				}
 			}
 		}
-		/// If there is no unactive object in pool
+		///If there is no unactive object left in pool
 		{
 			//Create the needed object witth set position and rotation
 			GameObject newObject = Instantiate(Need, Position, Rotation);
 			//Set the new object parent as this object for organize
-			newObject.transform.parent = transform;
+			newObject.transform.SetParent(transform);
 			//Add it into pool list
 			objectsPool.Add(newObject);
 			//Set the new object active state

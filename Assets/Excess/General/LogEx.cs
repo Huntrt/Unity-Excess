7a6ce5f8e28206c2e6073bc9c632c350bdb6a9c0
Huntrt <huntrt.business@outlook.com>
2021-12-15 00:01:50 +0700

    Rework game log into logex

---
+++ Assets/Excess/General/LogEx.cs
@@ -0,0 +1,61 @@
+using System.Collections; 
+using UnityEngine;
+
+namespace Excess
+{
+public class LogEx : MonoBehaviour
+{
+	//Turn this script into singleton
+	public static LogEx Debug; void Awake() {Debug = this;}
+	//Current log data
+	[SerializeField][TextArea] string logData;
+	//The line limit and line count
+	[SerializeField] int lineLimit; int lineCount;
+	//UI to display log
+	public TMPro.TextMeshProUGUI display; 
+
+	void Start()
+	{
+		//If this object has an TMP Text to display
+		if(GetComponent<TMPro.TextMeshProUGUI>() != null)
+		{	
+			//Get the TMP Text component
+			display = GetComponent<TMPro.TextMeshProUGUI>();
+		}
+		//Send an error if this object don't has TMP Text to display
+		else {UnityEngine.Debug.LogError("The Log Excess don't has any TextUGUI");}
+	}
+
+	public void Log(object info)
+	{
+		//Adding info to log text
+		logData += info + "\n";
+		//Increase one line
+		lineCount++;
+		//Start remove old log after go out of line limit
+		if(lineCount > lineLimit) {RemoveLog();}
+		UpdateLog();
+	}
+
+	void RemoveLog()
+	{
+		//Delete the first line
+		logData = logData.Substring(logData.IndexOf("\n")+1);
+		//Decrease one line
+		lineCount--;
+		UpdateLog();
+	}
+
+	public void ClearLog()
+	{
+		//Remove all the log data
+		logData = "";
+		//Remove all the line count
+		lineCount -= lineCount;
+		UpdateLog();
+	}
+
+	//Update log text UI
+	void UpdateLog() {display.text = logData;}
+}
+}
---
+++ Assets/Sample/LogTesting.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class LogTesting : MonoBehaviour
+{
+	//Print random test log
+    public void PrintTest() {Excess.LogEx.Debug.Log("test - " + Random.Range(0,100));}
+}

aa7bf172fb1ce3081e707d3d31fa3f63c4101fca
Huntrt <huntrt.business@outlook.com>
2021-12-14 21:37:27 +0700

    Create button cycling

--- Assets/Excess/UI/ButtonCycling.cs
+++ Assets/Excess/UI/ButtonCycling.cs
@@ -1,18 +1,40 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ButtonCycling : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	//List of gameobject to cycle
+    public List<GameObject> cycleList = new List<GameObject>();
+	public int currentCycle;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	//Display the current cycle
+	void Start() {DisplayCycle();}
+
+	public void NextCycle()
+	{
+		//Go to the next current cycle 
+		currentCycle++;
+		//Reset back to the first cycle if has go through all of cyle
+		if(currentCycle >= cycleList.Count) {currentCycle = 0;}
+		//Display the current cycle
+		DisplayCycle();
+	}
+
+	public void BackCycle()
+	{
+		//Go back to the current cycle 
+		currentCycle--;
+		//Reset back to the final cycle if has go back all of cyle
+		if(currentCycle < 0) {currentCycle = cycleList.Count-1;}
+		//Display the current cycle
+		DisplayCycle();
+	}
+
+	public void DisplayCycle()
+	{
+		//Deactive all the object in cycle list
+		foreach (GameObject c in cycleList) {c.SetActive(false);}
+		//Display only the current cycle object
+		cycleList[currentCycle].SetActive(true);
+	}
 }

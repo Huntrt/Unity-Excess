addd8374c851d0cf1a3e4f63ed404c10d2d59547
Huntrt <huntrt.business@outlook.com>
2022-01-03 23:22:02 +0700

    Make the pooler use singleton instead of static

--- Assets/Excess/Gameplay/Pool.cs
+++ Assets/Excess/Gameplay/Pool.cs
@@ -5,35 +5,37 @@ namespace ExcessPackage
 {
 public class Pool : MonoBehaviour
 {
+	//Make the pool component into singleton
+	public static Pool s; void Awake() {s = this;}
 	//The pool contain all the object has create
-	public static List<GameObject> objectsPool = new List<GameObject>();
+	public List<GameObject> poolList = new List<GameObject>();
 
 	//Create the object needed with wanted position, rotation, does it auto active upon create? and do it need to has parent?
-    public static GameObject Create(GameObject Need, Vector3 Position, Quaternion Rotation, bool autoActive = true, Transform parent = null)
+    public GameObject Create(GameObject Need, Vector3 Position, Quaternion Rotation, bool autoActive = true, Transform parent = null)
     {
 		///If there is unactive object in pool
-        if(objectsPool.Count > 0)
+        if(poolList.Count > 0)
 		{
 			//Go through all the object in pool in reverse order
-			for (int i = objectsPool.Count-1; i >= 0; i--)
+			for (int i = poolList.Count-1; i >= 0; i--)
 			{
 				//Remove any null object left from pool then skip it from being check
-				if(objectsPool[i] == null) {objectsPool.RemoveAt(i); continue;}
+				if(poolList[i] == null) {poolList.RemoveAt(i); continue;}
 				//Remove the "(Clone)" out of gameobject from pool name
-				string objectName = objectsPool[i].name.Replace("(Clone)","");
+				string objectName = poolList[i].name.Replace("(Clone)","");
 				//If there is an unactive object in pool with the same name of object need to get
-				if(!objectsPool[i].activeInHierarchy && string.Equals(Need.name, objectName))
+				if(!poolList[i].activeInHierarchy && string.Equals(Need.name, objectName))
 				{
 					//Set the polled object position
-					objectsPool[i].transform.position = Position;
+					poolList[i].transform.position = Position;
 					//Set the polled object rotation
-					objectsPool[i].transform.rotation = Rotation;
+					poolList[i].transform.rotation = Rotation;
 					//Set the pooled object parent from parameter
-					objectsPool[i].transform.SetParent(parent);
+					poolList[i].transform.SetParent(parent);
 					//Active it depend if need to active
-					objectsPool[i].SetActive(autoActive);
+					poolList[i].SetActive(autoActive);
 					//Return it that object and no need to continue code
-					return objectsPool[i];
+					return poolList[i];
 				}
 			}
 		}
@@ -44,7 +46,7 @@ public class Pool : MonoBehaviour
 			//Set the new object parent from parameter
 			newObject.transform.SetParent(parent);
 			//Add it into pool list
-			objectsPool.Add(newObject);
+			poolList.Add(newObject);
 			//Set the new object active state
 			newObject.SetActive(autoActive);
 			//Return the new object to

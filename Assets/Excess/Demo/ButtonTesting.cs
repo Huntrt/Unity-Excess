b64072b234ce892305d650ebed186a7a6a8454eb
Huntrt <huntrt.business@outlook.com>
2022-04-10 21:00:39 +0700

    Fixing button when interactable are disable

--- Assets/Excess/Demo/ButtonTesting.cs
+++ Assets/Excess/Demo/ButtonTesting.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using System.Collections;
 using ExcessPackage.UI;
 using ExcessPackage;
 using UnityEngine;
@@ -14,10 +12,5 @@ public class ButtonTesting : MonoBehaviour
 		button = GetComponent<ButtonExcess>();
 		//Log the state when it got change
 		button.onStateChange.AddListener((ButtonState s) => {LogEx.Debug.Log(s.ToString());});
-		//Log when released button
-		// button.onStateChange.AddListener
-		// ((ButtonState s) => {if(s == ButtonState.Released) LogEx.Debug.Log("Released");});
-		//Log when pressed button
-		button.onClick.AddListener(() => {LogEx.Debug.Log("Pressed");});
 	}
 }
--- Assets/Excess/UI/ButtonExcess.cs
+++ Assets/Excess/UI/ButtonExcess.cs
@@ -1,11 +1,10 @@
 using UnityEngine.EventSystems;
-using System.Collections;
 using UnityEngine.Events;
 using UnityEngine.UI;
 using UnityEngine;
 using System;
 
-namespace ExcessPackage { namespace UI 
+namespace ExcessPackage { namespace UI
 {
 //All the state of button
 public enum ButtonState {Normal, Highlighted, Pressed, Selected, Disabled, Holded, Released}
@@ -14,20 +13,21 @@ public class ButtonExcess : Selectable
 {
 	//An replicate of the default button on click event
 	public Button.ButtonClickedEvent onClick;
+	[Serializable] public class StateChanging : UnityEvent<ButtonState> {} //The event that send state
+	[Serializable] public class Toggling : UnityEvent<bool> {} //The event that toggle state
 	[Tooltip("The event when button state change")] public StateChanging onStateChange = new StateChanging();
 	[Tooltip("The event when button toggle change")] public Toggling onToggle = new Toggling();
 	[Tooltip("The button's current state")] public ButtonState currentState; public bool areToggle;
 	ButtonState defaultState; //The state where the button will revert back to
-	[Serializable] public class StateChanging : UnityEvent<ButtonState> {} //The event that send state
-	[Serializable] public class Toggling : UnityEvent<bool> {} //The event that toggle state
-
 	///When the button state are change
-	protected override void DoStateTransition(SelectionState state, bool instant)
+	protected override void DoStateTransition(SelectionState transition, bool instant)
 	{
-		//Run the Selectable's DoStateTransition function
-		base.DoStateTransition(state, instant);
-		//Check the state when transitioning to save the default state
-		switch(state)
+		//Run the Selectable's DoStateTransition function and get the state it transition to
+		base.DoStateTransition(transition, instant);
+		/// Stop if button are not interactable then update state to disable
+		if(!interactable) {UpdateState("Disabled"); return;}
+		//Saving the default state when transition
+		switch(transition)
 		{
 			//The default state are now normal when transition to normal
 			case SelectionState.Normal: defaultState = ButtonState.Normal; break;
@@ -36,7 +36,7 @@ public class ButtonExcess : Selectable
 			//The default state are now selected when transition to selected
 			case SelectionState.Selected: defaultState = ButtonState.Selected; break;
 		}
-		//Don't change state if the current state are holding
+		//Don't change state if the currently holding
 		if(currentState == ButtonState.Holded) {return;}
 		//If transition to pressed
 		if(transition == SelectionState.Pressed)
@@ -68,6 +68,8 @@ public class ButtonExcess : Selectable
 	{
 		//Run the Selectable's OnPointerUp function
 		base.OnPointerUp(eventData);
+		/// Stop if button are not interactable then update state to disable
+		if(!interactable) {UpdateState("Disabled"); return;}
 		//No longer holding button
 		Holding(false);
 		//Change the state to release
@@ -80,6 +82,7 @@ public class ButtonExcess : Selectable
 	///Updating the button excess state
 	void UpdateState(string StateSet)
 	{
+		print(StateSet);
 		//Update the current state to the state has given
 		currentState = (ButtonState)Enum.Parse(typeof(ButtonState), StateSet.ToString());
 		//Send an event with the current state
